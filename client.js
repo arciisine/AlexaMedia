1565ea65a2269dfc6148b065f3c145a1d5984587
Timothy Soehnlin <tim@eaiti.com>
2016-01-16 23:39:24 -0600

    Moving to firebase, should be easier on the raspberry pi

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
-var myFirebaseRef = 
-new Firebase("https://publicdata-cryptocurrency.firebaseio.com/dogecoin");
-lcd.on("ready", function() {
+var Firebase = require('firebase');
+var ref = new Firebase("https://scorching-fire-5002.firebaseio.com/actions");
+
+ref.authWithCustomToken('', function(err, res) {
   // Listen for changes to the Bitcoin price
-  myFirebaseRef.child("last").on("value", function(snap) {
+  ref.on("child_added", function(item) {
     // Print the value on the LCD
-    lcd.cursor(1, 1).clear().print("Doge: " + snap.val());
-  });
+    console.log(item.val().action);
+    ref.child(item.key()).remove();
+  });  
 });
