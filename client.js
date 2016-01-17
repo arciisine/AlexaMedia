4e5dc53266a34304941baf9856bd18378bf78024
Timothy Soehnlin <tim@eaiti.com>
2016-01-16 23:42:17 -0600

    Excluding config

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
 var Firebase = require('firebase');
-var ref = new Firebase("https://scorching-fire-5002.firebaseio.com/actions");
+var config = JSON.parse(require('fs').readFileSync('config.json'));
+var ref = new Firebase("https://"+config.namespace+".firebaseio.com/"+config.topic);
 
-ref.authWithCustomToken('', function(err, res) {
+ref.authWithCustomToken(config.secret, function(err, res) {
   // Listen for changes to the Bitcoin price
   ref.on("child_added", function(item) {
     // Print the value on the LCD
