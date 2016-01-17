488e11dfaf20912779daed7bd5171fa036325e1b
Timothy Soehnlin <tim@eaiti.com>
2016-01-17 15:21:04 -0600

    Moving to use amazon's sdk for node based alexa handlers

--- client/listener.js
+++ client/listener.js
@@ -1,6 +1,6 @@
 var Firebase = require('firebase');
 
-var config = JSON.parse(require('fs').readFileSync(__dirname + '/../config.json'));
+var config = JSON.parse(require('fs').readFileSync(__dirname + '/config.json'));
 var ref = new Firebase("https://"+config.namespace+".firebaseio.com/"+config.topic);
 
 var listeners = [];
